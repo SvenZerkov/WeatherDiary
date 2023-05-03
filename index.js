e8e144e26b313234d4b75b45e56e4f77c0ac8299
RosaHaa <rosa.haahtikari@student.hamk.fi>
2023-05-03 11:42:30 +0300

    CSS changes

--- index.js
+++ index.js
@@ -39,12 +39,14 @@ const dbURI = 'mongodb+srv://Team12:WeatherDiary2023@weatherdiary.fssyihy.mongod
 
 app.get('/', async (req, res) => {
     const weatherInfo = {};
+    const UserNotesEmpty = {};
     
         res.render('index',
         {
             pagetitle: "WeatherDiary",
             desc: "At this website, you can view historical weather data for Helsinki and add your own personal notes regarding specific dates. Please note that the weather data from today and the past couple of days may be missing due to delays. We retrieve our weather information from the Open Meteo API. At the bottom of this page, you can enter your own comments. To begin, please select a date between January 1, 2000, and today. Once you have chosen your preferred date, click on the 'View' button.",
             weatherDetails: weatherInfo,
+            UserNotes: UserNotesEmpty,
         });
     //})
 });
