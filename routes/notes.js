4fa16b597c3a3b3d249b23bf726c09c971c00d0e
SanteriV <54982640+SvenZerkov@users.noreply.github.com>
2023-05-04 11:17:43 +0300

    controllers, routes comment

--- routes/notes.js
+++ routes/notes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const noteController = require("../controllers/notes.js");
 
-/* router.get("/", noteController.home); */
+/* 
+router.get("/", noteController.home); 
+router.post("/", noteController.newHome);
+router.get("/api/notes/(:id)", noteController.getNote);
+router.get("/api/notes", noteController.getAll);
+*/
 
 module.exports = router;
